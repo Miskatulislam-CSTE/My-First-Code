49797362f8c39ef49874a7348b0f75f33128cc3e
Miskatul Islam <miskatulislam2006@gmail.com>
2026-02-22 22:30:11 +0600

    Add files via upload

---
+++ Loop C-Program/lokus series.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()   //Locus series start is 2 1
+{
+    int n,i,first=2,second=1,next ;
+    printf("Enter any number: ");
+    scanf("%d",&n);
+    printf("%d %d ",first,second);
+    for(i=1; i<=n; i++)   // 2 1 3 4 7 ......
+    {
+        next = first+second ;
+        printf("%d ",next);
+        first = second ;
+        second = next ;
+    }
+    printf("\n");
+    return 0 ;
+}
