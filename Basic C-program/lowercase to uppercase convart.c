9fc1c9a0f45b9cbbf56212d2d749c425a015e3c5
Miskatul Islam <miskatulislam2006@gmail.com>
2026-02-22 22:31:36 +0600

    Add files via upload

---
+++ Basic C-program/lowercase to uppercase convart.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+
+{
+    char lower;
+    printf("Enter the lowercase character: ");
+    scanf("%c",&lower);
+    printf("The upperecase character is: %c",lower-32);
+    return 0;
+}
