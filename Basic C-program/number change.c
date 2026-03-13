4bc10b0712778276f3699f775cd2207192e290dd
Miskatul Islam <miskatulislam2006@gmail.com>
2026-03-13 22:08:05 +0600

    Add files via upload

---
+++ Basic C-program/number change.c
@@ -0,0 +1,10 @@
+// decimal take octal change
+#include<stdio.h>
+int main()
+{
+    int number;
+    printf("Enter any decimal number= ");
+    scanf("%d",&number);
+    printf("octal number=%o",number);
+    return 0;
+}
